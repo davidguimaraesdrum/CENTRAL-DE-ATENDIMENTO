7a1dd2fc3de1be3f62c73bdced2b1057a99fd45c
davidguimaraesdrum <david.guima@hotmail.com>
2022-07-05 10:47:03 -0300

    A
    A

--- js/scripts.js
+++ js/scripts.js
@@ -20,41 +20,41 @@ let slideIndex = 0;
           setTimeout(showSlides, 4000); // Change image every 2 seconds
         }
 
-// ========================== DISABLE BOTÕES ==========================
+// ========================== DISABLE BOTÕES ========================== //
 
-function PLANTAO() {
-  const now = new Date(); //Método javascript para trabalhar com datas
-  let holyday = false; //Variável para dias que forem feriado (padrão sempre é false)
+const a = document.querySelector("#plantao");
 
-  let noon = [8, 9, 10, 11]; //Horário da manhã para domingos e feriados
-  let afternoon = [13, 14, 15, 16]; // Horário da tarde para sábados, domingos e feriados
-  let night = [18, 19, 20, 21]; // horário noturto padrão para todos os dias,
+const now = new Date(); //Método javascript para trabalhar com datas
+let holyday = false; //Variável para dias que forem feriado (padrão sempre é false)
 
-  //Domingo/Feriado
-  if(now.getDay() == 0 || holyday){ // Valida se é domingo ou se a variável holyday é true
-    if((noon.includes(now.getHours()) || afternoon.includes(now.getHours()) || night.includes(now.getHours()))){ //Valida se a hora atual está nos horários definidos
-      document.getElementById("plantao").disabled = false;	//Caso a validação anterior for verdadeira, o elemento que tem id plantao receberá false no campo disabled e o botão fica attivo
-    }else{
-      document.getElementById("plantao").disabled = true; //Caso a validação antetrior for falsa, o elemento recebe true, deixando o botão inativo
-    }
+let noon = [8, 9, 10, 11]; //Horário da manhã para domingos e feriados
+let afternoon = [13, 14, 15, 16]; // Horário da tarde para sábados, domingos e feriados
+let night = [18, 19, 20, 21]; // horário noturto padrão para todos os dias,
+
+//Domingo/Feriado
+if(now.getDay() == 0 || holyday){ // Valida se é domingo ou se a variável holyday é true
+  if((noon.includes(now.getHours()) || afternoon.includes(now.getHours()) || night.includes(now.getHours()))){ //Valida se a hora atual está nos horários definidos
+    document.getElementById("plantao").style.visibility = 'visible';//Caso a validação anterior for verdadeira, o elemento que tem id plantao receberá false no campo disabled e o botão fica attivo
+  }else{
+    document.getElementById("plantao").style.visibility = "hidden"; //Caso a validação antetrior for falsa, o elemento recebe true, deixando o botão inativo
   }
+}
 
-  //Semana
-  if((now.getDay() > 0 && now.getDay() < 6) && !holyday){ //Valida se é entre segunda e sexta e não é feriado
-    if(night.includes(now.getHours())){
-      document.getElementById("plantao").disabled = false;	
-    }else{
-      document.getElementById("plantao").disabled = true;
-    }
+//Semana
+if((now.getDay() > 0 && now.getDay() < 6) && !holyday){ //Valida se é entre segunda e sexta e não é feriado
+  if(night.includes(now.getHours())){
+    document.getElementById("plantao").style.visibility = 'visible';
+  }else{
+    document.getElementById("plantao").style.visibility = "hidden";
   }
+}
 
-  //Sabado
-  if((now.getDay() == 6) && !holyday){ //Valida se é sabado e não é feriado
-    if(afternoon.includes(now.getHours()) || night.includes(now.getHours())){ 
-      document.getElementById("plantao").disabled = false;	
-    }else{
-      document.getElementById("plantao").disabled = true;
-    }
+//Sabado
+if((now.getDay() == 6) && !holyday){ //Valida se é sabado e não é feriado
+  if(afternoon.includes(now.getHours()) || night.includes(now.getHours())){ 
+    document.getElementById("plantao").style.visibility = 'visible';
+  }else{
+    document.getElementById("plantao").disabled = true;
   }
 }
 // ==========================  ==========================
